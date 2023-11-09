0740180e16c811eafdc1035c201f4c308d48a32f
Samuel Hartmann <hartmann.samuel@gmx.de>
2023-11-09 22:40:52 +0100

    enable toggling of todo done visually

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,13 +1,19 @@
-// import './Todo.css'
+import './Todo.css'
 
 function Todo({thisTodo, setTodos}) {
     const deleteTodo = (e) => {
         setTodos(prevTodos => prevTodos.filter(todo => thisTodo.id === todo.id ? null : todo))
     }
 
+    const handleChange = (e) => {
+        e.target.checked && setTodos(prevTodos => prevTodos.map(todo => thisTodo.id === todo.id ? {...todo, done: true } : todo))
+        !e.target.checked && setTodos(prevTodos => prevTodos.map(todo => thisTodo.id === todo.id ? {...todo, done: false } : todo))
+    }
+
     return (
         <>
-        <div className="todo" id={`todo-${thisTodo.id}`}>
+        <div className={`todo ${thisTodo.done ? 'done' : 'active'}`} id={`todo-${thisTodo.id}`}>
+            <input type="checkbox" name="task" id={`task-${thisTodo.id}`} onChange={handleChange} checked={thisTodo.done} />
             <span>{thisTodo.task}</span>
             <button type="button" onClick={(e) => deleteTodo(e)}>&#10005;</button>
         </div>
