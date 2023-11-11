934eb9c246a2e214595bf524184b2484ab9ae175
Samuel Hartmann <hartmann.samuel@gmx.de>
2023-11-11 08:43:48 +0100

    add 404 error fallback in express

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,11 @@ app.delete('/', (req, res) => {
 
 
 
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+  })
+
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
