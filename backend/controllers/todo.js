1664897eaa47eef682e983641771a072096a6125
Samuel Hartmann <hartmann.samuel@gmx.de>
2023-11-11 20:55:55 +0100

    improve error handling

--- backend/controllers/todo.js
+++ backend/controllers/todo.js
@@ -3,21 +3,34 @@ const Todo = require('../models/todo')
 
 const readTodos = async (req, res) => {
     const todos = await Todo.find({})
+
+    if(!todos){
+        return res.status(400).json({error: 'No todos'})
+    }
+
     res.status(200).json(todos)
 }
+
 
 const readTodo = async (req, res) => {
     const {id: todoId} = req.params
 
     const todo = await Todo.findById(todoId)
+
+    if(!todo){
+        return res.status(400).json({error: 'No such todo'})
+    }
+
     res.status(200).json(todo)
 }
+
 
 const createTodo = async (req, res) => {
     const {task} = req.body
     const todo = await Todo.create({task})
     res.status(200).json(todo)
 }
+
 
 const updateTodo = async (req, res) => {
     try {
@@ -37,10 +50,11 @@ const updateTodo = async (req, res) => {
     
         res.status(200).json(updatedTodo);
     } catch (error) {
-        console.error('Error toggling todo:', error);
+        console.error('Error:', error);
         res.status(500).send('Internal Server Error');
     }      
 }
+
 
 const deleteTodo = async (req, res) => {
     try {
@@ -55,7 +69,7 @@ const deleteTodo = async (req, res) => {
     
         res.status(200).json(deleteTodo);
     } catch (error) {
-        console.error('Error toggling todo:', error);
+        console.error('Error:', error);
         res.status(500).send('Internal Server Error');
     }      
 }
