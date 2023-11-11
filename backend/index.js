d0bb1a821ccca44dcf27312d550551285ce767f1
Samuel Hartmann <hartmann.samuel@gmx.de>
2023-11-11 12:26:52 +0100

    add local mongo URI as env variable

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ exp.use(express.json())
 main().then(() => {console.log('index.js connected to mongoose')}).catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/react-todo');
+    await mongoose.connect(`${process.env.MONGO_URI}`);
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
