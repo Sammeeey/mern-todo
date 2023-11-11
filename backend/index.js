17a0ce886383b21574f1f3d4f073efb9431e58ee
Samuel Hartmann <hartmann.samuel@gmx.de>
2023-11-11 09:23:34 +0100

    add comments & send json in get request

--- backend/index.js
+++ backend/index.js
@@ -8,18 +8,22 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 
 
+// read todo
 app.get('/', (req, res) => {
-    res.send('Hi')
+    res.json({"id":"44fb-4b53-4034-9a0c-e8ca44767982","task":"blabla","done":false})
 })
 
+// create todo
 app.post('/', (req, res) => {
     res.send('Get POST')
 })
 
+// check off todo
 app.put('/', (req, res) => {
     res.send('PUT req')
 })
 
+// delete todo
 app.delete('/', (req, res) => {
     res.send('DELETE req')
 })
