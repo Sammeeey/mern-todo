9422dc7d6046b719a3f1307c082cd0f0b26ab662
Samuel Hartmann <hartmann.samuel@gmx.de>
2023-11-11 17:56:36 +0100

    create basic todo schema

---
+++ backend/models/Todo.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+
+const todoSchema = new Schema({
+    // {"id":"44fb-4b53-4034-9a0c-e8ca44767982","task":"blabla","done":false}
+    task: {
+        type: String,
+        required: [true, 'task name is required']
+    },
+    done: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Todo', todoSchema)
